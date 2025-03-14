a742875104707bd2da9de32a1a6681f49145b13a
Sofi Brugo <110871256+Sbrugo@users.noreply.github.com>
2025-03-14 11:34:34 -0300

    Jest

--- src/modules/services/users.service.spec.ts
+++ src/modules/services/users.service.spec.ts
@@ -3,16 +3,40 @@ import { UsersService } from '@services/users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userModel: Model<User>;
 
+    const mockUserModel = {
+    save: jest.fn().mockImplementation(function () {
+      return Promise.resolve({ ...this, id: '1' });
+    }),
+    findById: jest.fn().mockResolvedValue({ id: '1', username: 'testuser', email: 'test@example.com' }),
+  };
+  
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, { provide: getModelToken(User), useValue: mockUserModel },],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userModel = module.get<Model<User>>(getModelToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a user and return it', async () => {
+    const dto: CreateUserDto = { username: 'testuser', email: 'test@example.com' };
+
+    jest.spyOn(userModel, 'constructor').mockImplementation(() => mockUserModel);
+
+    const result = await service.createUser(dto);
+
+    expect(result).toEqual({
+      ...dto,
+      id: '1',
+    });
+
+    expect(mockUserModel.save).toHaveBeenCalled(); 
   });
 });
